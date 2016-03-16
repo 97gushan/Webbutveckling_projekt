a879b2370385623209213dc3d4672b9d86683e12
Gustav Hansson <97gushan@bk-s2722x.local>
2016-03-16 09:52:47 +0100

    testing some database handling

--- js/main.js
+++ js/main.js
@@ -6,10 +6,17 @@ function sign_up(){
         mail: $("#mail").val()
 
     };
+    // save to a database
+    //$.post("http://97gushan.pythonanywhere.com/formular/", {"name":input["name"], "mail":input["mail"]});
+
+    test = $.get("http://97gushan.pythonanywhere.com/wubwub/");
+
+    console.log(test[lista["foo"]]);
 
     $(".input_box").hide();
     $(".pre_sign_up_text").hide();
     $(".sign_up_text").show();
     $(".sign_btn").hide();
+
 
 }
