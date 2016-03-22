179ce6d6ee834b9f58fbd29bf3f3eda7c06a8625
97gushan <97gustavh@gmail.com>
2016-03-22 10:08:42 +0100

    tes

--- js/main.js
+++ js/main.js
@@ -6,12 +6,6 @@ function sign_up(){
         mail: $("#mail").val()
 
     };
-    // save to a database
-    //$.post("http://97gushan.pythonanywhere.com/formular/", {"name":input["name"], "mail":input["mail"]});
-
-    test = $.get("http://97gushan.pythonanywhere.com/wubwub/");
-
-    console.log(test[lista["foo"]]);
 
     $(".input_box").hide();
     $(".pre_sign_up_text").hide();
